85bd261708832ddb28f92857d4991bdf6665a09d
Jorge Cimentada <cimentadaj@gmail.com>
2016-05-13 17:59:23 +0200

    Update 8 - Multicollinearity_ and_testing_nested_models.R

--- Coursera_regression_models/8 - Multicollinearity_ and_testing_nested_models.R
+++ Coursera_regression_models/8 - Multicollinearity_ and_testing_nested_models.R
@@ -17,6 +17,11 @@ library(car)
 fit <- lm(Fertility ~ ., data=swiss)
 vif(fit)
 
+## How do you interpret this? For instance,
+## the variance in the estimated coefficient of Education is 2.774943 times what it might have been if Education were not
+## correlated with the other regressors. Since Education and score on an Examination are likely to be correlated, we might
+## guess that most of the variance inflation for Education is due to including Examination.
+
 # The VIF is the variance inflation factor(VIF) and if it's above 5 it means that you have
 # a problem of multicollinearity with this variable and others.
 
@@ -43,55 +48,3 @@ anova(fit1,fit3,fit5)
 
 ## Ideally each model should have 1 additional variable but if you know the dataset then you
 ## can play around.
-
-## These three functions make a simulation with uncorrelated and correlated regressions to exemplify how the variation across models
-## can have a significant impact over coefficient variation
-
-makelms <- function(x1, x2, x3){
-  # Simulate a dependent variable, y, as x1
-  # plus a normally distributed error of mean 0 and 
-  # standard deviation .3.
-  y <- x1 + rnorm(length(x1), sd = .3)
-  # Find the coefficient of x1 in 3 nested linear
-  # models, the first including only the predictor x1,
-  # the second x1 and x2, the third x1, x2, and x3.
-  c(coef(lm(y ~ x1))[2], 
-    coef(lm(y ~ x1 + x2))[2], 
-    coef(lm(y ~ x1 + x2 + x3))[2])
-}
-
-# Regressor generation process 1.
-rgp1 <- function(){
-  print("Processing. Please wait.")
-  # number of samples per simulation
-  n <- 100
-  # number of simulations
-  nosim <- 1000
-  # set seed for reproducibility
-  set.seed(4321)
-  # Point A
-  x1 <- rnorm(n)
-  x2 <- rnorm(n)
-  x3 <- rnorm(n)
-  # Point B
-  betas <- sapply(1 : nosim, function(i)makelms(x1, x2, x3))
-  round(apply(betas, 1, var), 5)
-}
-
-# Regressor generation process 2.
-rgp2 <- function(){
-  print("Processing. Please wait.")
-  # number of samples per simulation
-  n <- 100
-  # number of simulations
-  nosim <- 1000
-  # set seed for reproducibility
-  set.seed(4321)
-  # Point C
-  x1 <- rnorm(n)
-  x2 <- x1/sqrt(2) + rnorm(n) /sqrt(2)
-  x3 <- x1 * 0.95 + rnorm(n) * sqrt(1 - 0.95^2)
-  # Point D
-  betas <- sapply(1 : nosim, function(i)makelms(x1, x2, x3))
-  round(apply(betas, 1, var), 5)
-}
