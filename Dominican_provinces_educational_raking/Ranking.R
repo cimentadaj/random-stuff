599b753369476163bb9e629780a39f8d0ef95cdf
Jorge Cimentada <cimentadaj@gmail.com>
2016-08-22 01:49:24 +0200

    Update Ranking.R

--- Dominican_provinces_educational_raking/Ranking.R
+++ Dominican_provinces_educational_raking/Ranking.R
@@ -12,13 +12,13 @@ analfabeto <- read.csv("analfabeto-provincia.csv", skip=4, nrow=33, stringsAsFac
 names(analfabeto)[1] <- c("Province") ## Make sure all columns are identifiable
 
 ## The goal is to estimate how big is a gap for each province, regardless of whether women or men are higher.
-analfabeto$gender_gap_analfa <- round((abs(analfabeto$Hombre - analfabeto$Mujer)/analfabeto$Total) * 100,1)
+analfabeto$gender_gap_analfa <- round((abs(analfabeto$Hombre - analfabeto$Mujer)/analfabeto$Total) * 100, 1) # Percentage of men which are analfabetos relative to women
 analfabeto[order(analfabeto$gender_gap_analfa), ]
 ## This will work as a rank. The higher the worst.
 
 ## original dataset: http://one.gob.do/Multimedia/Download?ObjId=1349
 yrsedu <- read.csv("anoseducacion.csv", skip=5, header=TRUE, nrow=32, stringsAsFactors = F)[2:3]
-yrsedu[2] <- lapply(yrsedu[2], round, 2) ## Let's make the numbers easier to understand
+yrsedu[2] <- round(yrsedu[2], 2)
 names(yrsedu)[1:2] <- c("Province","avg_yrs_education") ## Make sure all columns are identifiable
 
 ## original dataset: http://one.gob.do/Multimedia/Download?ObjId=5985
@@ -43,21 +43,33 @@ names(repetition) <- c("Province", "Inic_dropout","Inic_promoted","Basic_dropout
 ## Make sure all columns are identifiable
 
 ## Delete unwanted columns
-repetition$Inic_promoted <- NULL
-repetition$Basic_promoted <- NULL
-repetition$Middle_promoted <- NULL
+delete_cols <- function(x) {
+    repetition <- repetition[,-x]
+    return(repetition)
+}
+
+repetition <- delete_cols(which(names(repetition) %in% c("Inic_promoted","Basic_promoted","Middle_promoted")))
 
 ## compile list with all data frames
 ourlist <- list(analfabeto, yrsedu, prof_province, repetition)
 
 ## Let's sort all Province columns from the data frames
 ourlist <- lapply(ourlist, function(x) x[order(x$Province),])
+ourlist[[4]]$Province <- gsub("? $", "", ourlist[[4]]$Province) # Delete white space at the end
 
-## Replace all Province names of data frame 4 with the names of the first one(which are all correctly
-## spelled)
-for (i in 1:length(ourlist[[1]][,1])) {
-    ourlist[[4]][,1][i] <- ourlist[[1]][,1][i] 
-}
+provinces <- do.call(cbind, lapply(ourlist, function(x) x$Province))
+
+
+# Check to see if the names of each province are the same
+provinces[,1] == provinces[,2:4] # row 25 is different
+provinces[,2] == provinces[,3:4] # row 25 is different
+provinces[,3] == provinces[,4, drop=F] # row 25
+
+# The difference is because of a capital letter(something common in Spanish). Confirm it:
+provinces[25,]
+# Fix it:
+ourlist[[3]][25,1] <- ourlist[[1]][25,1]
+lapply(ourlist, function(x) x[25,1]) # confirmation
 
 ## Let's bind all data frame together, with only the province variable of the first one
 complete_data <- cbind(ourlist[[1]][c(1,5)],ourlist[[2]][-1],ourlist[[3]][-(1:3)],ourlist[[4]][,-1])
@@ -80,6 +92,6 @@ complete_data2 <- complete_data[,columns]
 ## Sum it all up to create an overall ranking
 complete_data2$total_ranking <- rowSums(complete_data2[-1])
 
-cl <- complete_data2[order(complete_data2$total_ranking),1, drop=F]
+cl <- complete_data2[order(complete_data2$total_ranking),1 , drop=F]
 row.names(cl) <- 1:nrow(cl)
 cl ## Final Ranking
