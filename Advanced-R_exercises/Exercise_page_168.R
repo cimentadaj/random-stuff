3cae0f3aea330ef029b85f8aaae916abe9d94b87
Jorge Cimentada <cimentadaj@gmail.com>
2016-09-21 14:35:33 +0200

    Create Exercise_page_168.R

---
+++ Advanced-R_exercises/Exercise_page_168.R
@@ -0,0 +1,21 @@
+# 1.
+
+message2error <- function(code) {
+  withCallingHandlers(code, message = function(e) stop(e))
+}
+
+message2error(stop("!"))
+#  Error in withCallingHandlers(code, message = function(e) stop(e)) : ! 
+traceback()
+
+message2error <- function(code) {
+  tryCatch(code, message = function(e) stop(e))
+}
+
+message2error(stop("!"))
+#  Error in doTryCatch(return(expr), name, parentenv, handler) : ! 
+traceback()
+
+# The error messages is called from the message argument but you can't see that
+# from the tryCatch. Also, tryCatch goes deeper into the function call which produced
+# the error whereas withCallingHandlers just presents the call from withCallingHandlers
