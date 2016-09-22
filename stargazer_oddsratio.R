3d050920d6daf3a1965ee2f66f6fa79f39c2e236
Jorge Cimentada <cimentadaj@gmail.com>
2016-09-22 19:08:23 +0200

    Create stargazer_oddsratio.R

---
+++ stargazer_oddsratio.R
@@ -0,0 +1,21 @@
+# This function computes the odds ratio from the log odds of an glm object. It only accepts lists
+# as the first argument because I didn't want to go into differentiating between single models, several models and lists.
+# The output is a stargazer table with the correct odds ratios, the correct standard errors and the previous p values.
+
+# This doesn't apply to svy glms and accepts all the arguments of stargazer.
+
+# Any pull requests are welcome!
+
+stargazer2 <- function(model, odd.ratio = F, ...) {
+  
+  stopifnot(class(model) == "list")
+  if (odd.ratio) {
+    coefOR2 <- lapply(model, function(x) exp(coef(x)))
+    seOR2 <- lapply(model, function(x) exp(coef(x)) * se.coef(x))
+    p2 <- lapply(model, function(x) summary(x)$coefficients[, 4])
+    stargazer(model, coef = coefOR2, se = seOR2, p = p2, ...)
+    
+  } else {
+    stargazer(model, ...)
+  }
+}
