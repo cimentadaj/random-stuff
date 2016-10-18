6ed78b3ee14e51c4de889d6243ecdb8386d43d3d
Jorge Cimentada <cimentadaj@gmail.com>
2016-10-18 18:36:26 -0400

    Update stargazer_oddsratio.R

--- stargazer_oddsratio.R
+++ stargazer_oddsratio.R
@@ -35,7 +35,7 @@ robustse <- function(x, coef = c("logit", "odd.ratio", "probs")) {
     return(mod1) # return logit with robust SE's
     } else if (coef == "odd.ratio") {
     mod1[, 1] <- exp(mod1[, 1]) # return odd ratios with robust SE's
-    mod1[, 2] <- mod1[, 1] * mod[, 2]
+    mod1[, 2] <- mod1[, 1] * mod1[, 2]
     return(mod1)
     } else {
     mod1[, 1] <- (mod1[, 1]/4) # return probabilites with robust SE's
