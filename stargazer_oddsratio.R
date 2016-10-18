3717aff4673984b433b7067c2d9528f62b69a793
Jorge Cimentada <cimentadaj@gmail.com>
2016-10-18 17:03:30 -0400

    Update stargazer_oddsratio.R

--- stargazer_oddsratio.R
+++ stargazer_oddsratio.R
@@ -19,3 +19,25 @@ stargazer2 <- function(model, odd.ratio = F, ...) {
     stargazer(model, ...)
   }
 }
+
+                 
+# Estimates robust standad error for lm and glm and
+# returns coefficients as either logit, odd ratios or probabilities.
+# logits are default
+
+robustse <- function(x, coef = c("logit", "odd.ratio", "probs")) {
+    require(lmtest)
+    require(sandwich)
+    
+    sandwich1 <- function(object, ...) sandwich(object) * nobs(object) / (nobs(object) - 1) # Function calculates SE's
+    mod1 <- coeftest(x, vcov = sandwich1) # apply the function over the variance-covariance matrix
+    if (coef == "logit") {
+    return(mod1) # return logit with robust SE's
+    } else if (coef == "odd.ratio") {
+    mod1[, 1] <- exp(mod1[, 1]) # return odd ratios with robust SE's
+    return(mod1)
+    } else {
+    mod1[, 1] <- (mod1[, 1]/4) # return probabilites with robust SE's
+    return(mod1)
+    }
+}
