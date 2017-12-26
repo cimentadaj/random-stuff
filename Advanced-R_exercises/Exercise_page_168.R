f6560e1ae718d66ccecbdfc5c0c917316f6c3546
Jorge Cimentada <cimentadaj@gmail.com>
2017-12-26 12:48:05 +0100

    Update Exercise_page_168.R

--- Advanced-R_exercises/Exercise_page_168.R
+++ Advanced-R_exercises/Exercise_page_168.R
@@ -19,3 +19,7 @@ traceback()
 # The error messages is called from the message argument but you can't see that
 # from the tryCatch. Also, tryCatch goes deeper into the function call which produced
 # the error whereas withCallingHandlers just presents the call from withCallingHandlers
+
+# 26/12/2017
+# tryCatch gives the traceback from it's context, only showing the traceback THROUGH tryCatch
+# withCallingHandlers gives the traceback globally, following each function call, going deeper.
